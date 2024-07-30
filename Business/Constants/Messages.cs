8d548eea836697ee30ab7609274fd800e2f40afe
byrchard <batuhanrecep06@hotmail.com>
2024-07-31 01:30:40 +0300

    To avoid magic string, messages class added to project

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Door Successfully Added";
+        public static string ProductDeleted = "Door Successfully Deleted";
+        public static string ProductUpdated = "Door Successfully Updated";
+
+        public static string UserNotFound = "User not found";
+        public static string PasswordError = "Password is wrong";
+        public static string SuccessfulLogin = "Successfully Logged In";
+        public static string UserAlreadyExists = "User Already Exist";
+        public static string UserRegistered = "User Already Registered";
+        public static string AccessTokenCreated = "AccessToken Created Successfully";
+    }
+}
