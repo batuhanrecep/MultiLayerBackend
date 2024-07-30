e5a190526d180d06e795746b325a3c1c3c44f184
byrchard <batuhanrecep06@hotmail.com>
2024-07-31 02:27:53 +0300

    For category object, get all operation added to project

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        IDataResult<List<Category>> GetList();
+        //IDataResult<Category> GetById(int productId);
+        //IDataResult<List<Category>> GetListByCategory(int categoryId);
+        //IResult Add(Category product);
+        //IResult Update(Category product);
+        //IResult Delete(Category product);
+    }
+}
---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,62 @@
+﻿using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+
+namespace Business.Concrete
+{
+    public class CategoryManager:ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public IDataResult<List<Category>> GetList()
+        {
+            return new SuccessDataResult<List<Category>>(_categoryDal.GetList().ToList());
+        }
+
+        //public IDataResult<Category> GetById(int productId)
+        //{
+        //    return new SuccessDataResult<Category>(_categoryDal.Get(p => p.CategoryId == productId));
+        //}
+
+
+
+        //public IDataResult<List<Category>> GetListByCategory(int categoryId)
+        //{
+        //    return new SuccessDataResult<List<Category>>(_categoryDal.GetList(p => p.CategoryId == categoryId).ToList());
+        //}
+
+
+        //public IResult Add(Category category)
+        //{
+        //    _categoryDal.Add(category);
+        //    return new SuccessResult(Messages.ProductAdded);
+
+        //}
+
+
+        //public IResult Update(Category category)
+        //{
+        //    _categoryDal.Update(category);
+        //    return new SuccessResult(Messages.ProductUpdated);
+        //}
+
+
+        //public IResult Delete(Category category)
+        //{
+        //    _categoryDal.Delete(category);
+        //    return new SuccessResult(Messages.ProductDeleted);
+        //}
+    }
+}
