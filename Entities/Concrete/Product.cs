72201c7e9754ee16b38704dab31878dfd0e04a1b
byrchard <batuhanrecep06@hotmail.com>
2024-07-30 18:56:20 +0300

    properties created for product

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; } //Because of northwind db, we named this ProductId. I would do only Id
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
