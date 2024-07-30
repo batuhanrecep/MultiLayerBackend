b152fe1aed04f68a5a3dc2e0e61e0ebe85d783c6
byrchard <batuhanrecep06@hotmail.com>
2024-07-30 23:22:31 +0300

    .

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        
+        List<Product> GetList();
+        Product GetById(int productId);
+        List<Product> GetListByCategory(int categoryId);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
