94290e2d2c7f1598c8f0acbb49043a695463a77e
byrchard <batuhanrecep06@hotmail.com>
2024-08-02 01:27:10 +0300

    Validation tool added to invoke fluentvalidation

---
+++ Core/CrossCuttingConcerns/Validation/FluentValidation/ValidationTool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            ValidationResult result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+
+            //OLD WAY
+            //var result = validator.Validate(entity);
+            //if (!result.IsValid)
+            //{
+            //    throw new ValidationException(result.Errors);
+            //}
+        }
+    }
+}
