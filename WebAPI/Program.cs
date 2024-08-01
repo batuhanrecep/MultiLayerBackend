e38b394629e9a1f00700b582da5fce62149f6e34
byrchard <batuhanrecep06@hotmail.com>
2024-08-02 00:19:35 +0300

    For CORS and JWT authentication implementation, programcs coded

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,10 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Business.DependencyResolvers.Autofac;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,17 +12,55 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+//builder.Services.AddControllers().AddControllersAsServices();
 
+//Autofac
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>
     (builder => builder.RegisterModule(new AutofacBusinessModule()));
 
+//Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder
+            .WithOrigins("http://localhost:4444") // Replace with your frontend domain
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
+//builder.WebHost.UseUrls("https://localhost:4444");
+
+
+//JWT
+var Configuration = builder.Configuration;
+var tokenOptions = Configuration.GetSection(key: "TokenOptions").Get<TokenOptions>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidIssuer = tokenOptions.Issuer,
+        ValidAudience = tokenOptions.Audience,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+    };
+});
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -26,6 +68,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
