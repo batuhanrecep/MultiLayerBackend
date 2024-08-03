e080e7d1ee270a990a981dcab8545039a52361e5
byrchard <batuhanrecep06@hotmail.com>
2024-08-04 01:34:36 +0300

    Memory cache manager written

---
+++ Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,77 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.RegularExpressions;
+
+namespace Core.CrossCuttingConcerns.Caching.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        private IMemoryCache _cache;
+
+        public MemoryCacheManager()
+        {
+            _cache = ServiceTool.ServiceProvider.GetService<IMemoryCache>();
+        }
+        public T Get<T>(string key)
+        {
+            return _cache.Get<T>(key);
+        }
+
+        public object Get(string key)
+        {
+            return _cache.Get(key);
+        }
+
+        public void Add(string key, object data, int duration)
+        {
+            _cache.Set(key, data, TimeSpan.FromMinutes(duration));
+        }
+
+        public bool IsAdd(string key)
+        {
+            return _cache.TryGetValue(key, out _);//with underline, we don't care value is exist or not
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key); 
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            // (_cache.Keys) before .net core there was a method like beginning. But now .net core is not givings us cache object. To get this object we are writing these;
+            //Half of the code came from Stackoverflow and other half written by Engin Demirog.
+            //To get details of the code: https://www.udemy.com/course/net-core-c-sharp-kursu-2/learn/lecture/16393876#questions/8583358
+
+            var cacheEntriesCollectionDefinition = typeof(MemoryCache).GetProperty("EntriesCollection", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            var cacheEntriesCollection = cacheEntriesCollectionDefinition.GetValue(_cache) as dynamic;
+
+
+            List<ICacheEntry> cacheCollectionValues = new List<ICacheEntry>();
+
+            foreach (var cacheItem in cacheEntriesCollection)
+            {
+
+                ICacheEntry cacheItemValue = cacheItem.GetType().GetProperty("Value").GetValue(cacheItem, null);
+
+
+                cacheCollectionValues.Add(cacheItemValue);
+            }
+
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var keysToRemove = cacheCollectionValues.Where(d => regex.IsMatch(d.Key.ToString())).Select(d => d.Key).ToList();
+
+            foreach (var key in keysToRemove)
+            {
+                _cache.Remove(key);
+            }
+        }
+    }
+}
