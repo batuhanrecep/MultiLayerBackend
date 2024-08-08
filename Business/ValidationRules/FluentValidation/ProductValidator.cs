ee685a55f8c3734a154b9840e7c579050c0e2ea8
byrchard <batuhanrecep06@hotmail.com>
2024-08-08 18:20:00 +0300

    Typo on productvalidator

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -17,11 +17,11 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(p => p.UnitPrice).NotEmpty();
             RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(1);
             RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
-            RuleFor(p => p.ProductName).Must(StartWithA);
+            RuleFor(p => p.ProductName).Must(StartsWithA);
             //...
         }
 
-        private bool StartWithA(string arg)
+        private bool StartsWithA(string arg)
         {
             return arg.StartsWith("A");
         }
