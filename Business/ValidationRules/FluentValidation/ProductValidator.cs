4c00669ed67e84e01364446c11675ccb004271e8
byrchard <batuhanrecep06@hotmail.com>
2024-08-08 18:33:31 +0300

    .

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -14,7 +14,8 @@ namespace Business.ValidationRules.FluentValidation
         {
             // RuleFor(p => p.ProductName).NotEmpty().Length(2, 30);  - It can be written like this but, it's better to write validations one by one.
             // With this way, we can write messages for every one of them. (Its just example)
-             
+            // Also, these rules should be about the entities. (Do not try to connect these with db or mix the business codes with these) 
+
             RuleFor(p => p.ProductName).NotEmpty().WithMessage("Product cannot be empty"); //MAGIC STRING - Add message class for these
             RuleFor(p => p.ProductName).Length(2, 30);
             RuleFor(p => p.UnitPrice).NotEmpty();
